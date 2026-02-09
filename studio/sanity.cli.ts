7c06aef79decd08e0b8e5accfad695d150035699
Alex Zhou <alex.zzh.s7@gmail.com>
2026-02-09 14:18:39 +0900

    Add Sanity Studio deployment appId

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'lorzawgu3tvbzh4006raaqsn',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
